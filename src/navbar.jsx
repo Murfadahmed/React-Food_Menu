f5ce2a3c639c4e9898ba03a0afeecd7e1b5a247a
Murfad AHMED <malikmurfad361@gmail.com>
2023-09-22 00:12:19 +0500

    Update navbar.jsx
    sirf name change howa hai

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -4,9 +4,9 @@ function App() {
   return (
     <div className="App">
       <div className="navbar">
-        <h1>Delight Food's</h1>
+        <h1> MBBH Food</h1>
         <div className="heading">
-          <h2>Our Menu</h2>
+          <h2>Food Cart</h2>
         </div>
         <p>We make all kind of food || Desi || Fast Food || B.B.Q,s || And Much more you can explore</p>
       </div>
